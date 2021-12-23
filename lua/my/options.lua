bc504690bf6f2763175fffdd69f3aa7a08fafbc5
Mariusz Andrzejewski <50769252+mariuszandrzejewski@users.noreply.github.com>
2021-12-23 10:22:06 +0100

    init

---
+++ lua/my/options.lua
@@ -0,0 +1,30 @@
+local o = vim.opt
+
+o.backup = false
+o.clipboard = o.clipboard + { 'unnamedplus' }
+o.cmdheight = 2
+o.expandtab = true
+o.fileencoding = 'utf-8'
+o.mouse = 'a'
+o.number = true
+o.scrolloff = 8
+o.shiftwidth = 4
+o.showtabline = 2
+o.sidescrolloff = 8
+o.signcolumn = 'yes'
+o.splitbelow = true
+o.splitright = true
+o.tabstop = 4
+o.timeoutlen = 300
+o.ttimeoutlen = 10
+o.updatetime =  300
+o.whichwrap = o.whichwrap + { 
+	['h'] = true,
+	['l'] = true,
+	['<'] = true,
+	['>'] = true,
+	['['] = true,
+	[']'] = true
+}
+o.wrap = false
+o.writebackup = false
