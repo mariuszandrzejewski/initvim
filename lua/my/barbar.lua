8710fd611d94d0a799d46214ccd501bb23aa93f6
Mariusz Andrzejewski <50769252+mariuszandrzejewski@users.noreply.github.com>
2023-09-09 20:20:19 +0200

    change barbar close tab glyph

--- lua/my/barbar.lua
+++ lua/my/barbar.lua
@@ -75,7 +75,7 @@ vim.g.bufferline = {
   -- Configure icons on the bufferline.
   icon_separator_active = '▎',
   icon_separator_inactive = '▎',
-  icon_close_tab = '',
+  icon_close_tab = 'x',
   icon_close_tab_modified = '●',
   icon_pinned = '車',
 
